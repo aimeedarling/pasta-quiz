41770fc14c3eca27ae94172bec23a826784f81e4
aimee <127796444+aimeedarling@users.noreply.github.com>
2023-04-28 20:32:11 -0700

    changes after tutoring with phil

--- assets/script.js
+++ assets/script.js
@@ -4,13 +4,18 @@ const questionsEl = document.querySelector("#question-section")
 const finalScoreEl = document.querySelector(".final-score")
 const scoreboardEl = document.querySelector(".scoreboard")
 const startBtn = document.querySelector("#start")
-const initialsInput = document.querySelector("#Initials")
+const initialsInput = document.querySelector("#initials")
 const submitBtn = document.querySelector("#submit")
 const a = document.querySelector(".a")
 const b = document.querySelector(".b")
 const c = document.querySelector(".c")
 const d = document.querySelector(".d")
 const questionText = document.querySelector(".question-text")
+const scoreEl = document.querySelector("#score")
+const highScoreListEl = document.querySelector("#high-score-list")
+const goBackBtn = document.querySelector(".go-back")
+const clearBtn = document.querySelector(".clear")
+let highScores = []
 
 
 const q1 = document.querySelector(".q1")
@@ -66,11 +71,13 @@ function setTime() {
 }
 
 function displayQuestion() {
-    questionText.textContent = questions[index].question
-    a.textContent = questions[index].choices[0]
-    b.textContent = questions[index].choices[1]
-    c.textContent = questions[index].choices[2]
-    d.textContent = questions[index].choices[3]
+    if (questions[index].question) {
+        questionText.textContent = questions[index].question
+        a.textContent = questions[index].choices[0]
+        b.textContent = questions[index].choices[1]
+        c.textContent = questions[index].choices[2]
+        d.textContent = questions[index].choices[3]
+    }
 }
 
 a.addEventListener("click", nextQuestion)
@@ -84,49 +91,77 @@ function nextQuestion() {
     } else if (this.textContent === questions[index].answer) {
         alert("Correct")
         index++
-        displayQuestion()
     } else if (this.textContent !== questions[index].answer) {
         timeRemaining = timeRemaining - 6
         alert("Wrong")
         index++
+    }
+
+    if (index < questions.length) {
         displayQuestion()
+    } else {
+        endQuiz()
     }
 }
 
 function endQuiz() {
+    timerEl.textContent = "Time left: " + timeRemaining;
     clearInterval(timerId);
-    const score = timeRemaining;
+    scoreEl.textContent = timeRemaining;
     questionsEl.classList.add("hide")
     finalScoreEl.classList.remove("hide")
-    finalScore.innerHTML = `<p>Your final score is <spa>${score}</span>.</p>`
+    // finalScoreEl.innerHTML = `<p>Your final score is <spa>${score}</span>.</p>`
 }
 
 submitBtn.addEventListener("click", function () {
     const initials = initialsInput.value
 
-    localStorage.setItem("score", score)
-    localStorage.setItem("initials", initials)
+    highScores.push({
+        initials,
+        score: timeRemaining
+    })
+    localStorage.setItem("score", JSON.stringify(highScores))
     finalScoreEl.classList.add("hide")
     scoreboardEl.classList.remove("hide")
+    displayScoreboard()
+}
+)
+function displayScoreboard (){
+   if (localStorage.getItem("score")){
+    highScores = JSON.parse (localStorage.getItem("score"))
+   }
+   highScoreListEl.textContent = ""
+   for (let i = 0; i < highScores.length; i++) {
+        var li = document.createElement ("li")
+        li.textContent = highScores[i].initials+ " - " + highScores[i].score
+        highScoreListEl.appendChild(li)
+   }
+}
+
+goBackBtn.addEventListener("click", function () {
+    location.reload()
+})
+
+clearBtn.addEventListener("click", function(){
+    localStorage.clear()
+    highScoreListEl.textContent = ""
 }
 )
 
 const scores = JSON.parse(localStorage.getItem("score")) || [];
 scores.sort((a, b) => b.score - a.score);
 
-scores.forEach((scores, index) => {
-    const row = document.createElement("tr");
-    const rank = document.createElement("td");
-    const initials = document.createElement("td");
-    const scoreEl = document.createElement("td");
+// scores.forEach((scores, index) => {
+//     const row = document.createElement("tr");
+//     const rank = document.createElement("td");
+//     const initials = document.createElement("td");
+//     const scoreEl = document.createElement("td");
 
-    rank.textContent = index + 1;
-    initials.textContent = score.initials;
-    scoreEl.textContent = score.score;
+//     rank.textContent = index + 1;
+//     initials.textContent = score.initials;
+//     scoreEl.textContent = score.score;
 
-    row.appendChild(rank);
-    row.appendChild(initials);
-    row.appendChild(scoreEl);
-
-    scoreboardEl.appendChild(row);
-});
+//     row.appendChild(rank);
+//     row.appendChild(initials);
+//     row.appendChild(scoreEl);
+// });
